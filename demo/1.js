a9dc37f642c8a07bb530407d7d099e7e2d3762a1
leifufeng <1500447979@qq.com>
2019-12-14 00:04:07 +0800

    add file

---
+++ demo/1.js
@@ -0,0 +1,10 @@
+http://www.oicqzone.com/pc/2019041724516.html
+
+var data = "Some semi-long text for testing";
+var password = "some password";
+var ctObj = CryptoJS.AES.encrypt(data, password);
+var ctStr = ctObj.toString();
+
+a = '08111208'
+b = "Some semi-long text for testing"
+c = "some password"
